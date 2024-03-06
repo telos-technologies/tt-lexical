022eef03f653c15bb6cd99ca4be8b63cdbdd1daf
Said Akhmedbayev <said.akh@gmail.com>
2024-03-06 17:42:15 -0500

    removed comments from floating toolbar

--- packages/lexical-standalone-editor/dist/Editor.js
+++ packages/lexical-standalone-editor/dist/Editor.js
@@ -21,7 +21,7 @@ require('@lexical/react/LexicalRichTextPlugin');
 require('@lexical/react/LexicalTabIndentationPlugin');
 require('@lexical/react/LexicalTablePlugin');
 require('@lexical/react/useLexicalEditable');
-var Editor = require('./Editor-fb44446c.js');
+var Editor = require('./Editor-5ecaba85.js');
 require('react');
 require('@lexical/code');
 require('@lexical/link');
